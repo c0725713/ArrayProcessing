d9ffc5a6d6dbce6df376b53877e944ad25ddcbc2
Jashandeep Singh Dhillon <725713@student.tor.loc>
2019-04-18 14:37:08 -0400

    commit at 2:37PM

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -13,6 +13,30 @@ namespace ConsoleApp1
             int[] array1 = new int[100];
             int[] frequency = new int[100];
             int n, i, j, ctr;
-        }
+
+
+            Console.Write("Give the input of number of elements to be stored in the array :");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input {0} elements in the array :\n", n);
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("element - {0} : ", i);
+                array1[i] = Convert.ToInt32(Console.ReadLine());
+                frequency[i] = -1;
+            }
+            for (i = 0; i < n; i++)
+            {
+                ctr = 1;
+                for (j = i + 1; j < n; j++)
+                {
+                    if (array1[i] == array1[j])
+                    {
+                        ctr++;
+                        frequency[j] = 0;
+                    }
+                }
+
+            }
     }
 }
